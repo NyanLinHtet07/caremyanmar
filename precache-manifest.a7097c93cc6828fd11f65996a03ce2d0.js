c17e4e3d623fed29bf51649be29fced401789751
NyanLinHtet07 <67371605+NyanLinHtet07@users.noreply.github.com>
2021-07-19 22:45:11 +0630

    Add files via upload

---
+++ precache-manifest.a7097c93cc6828fd11f65996a03ce2d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bc3ac2fb4e78dc3ef6",
+    "url": "/css/app.e5695753.css"
+  },
+  {
+    "revision": "503e9cd8e0c7e1092e73",
+    "url": "/css/chunk-vendors.ff650d32.css"
+  },
+  {
+    "revision": "67a56ab42861b21c944f421d5157bbef",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "8981dd5dc315366b5d1518f3f169e77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52bc3ac2fb4e78dc3ef6",
+    "url": "/js/app.9f75a11c.js"
+  },
+  {
+    "revision": "503e9cd8e0c7e1092e73",
+    "url": "/js/chunk-vendors.743b0770.js"
+  },
+  {
+    "revision": "b0ac4ccbee2eb46ed4444f3674ea9097",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
